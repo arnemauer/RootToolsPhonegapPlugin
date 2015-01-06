04e57eba5994b18f42617cab33931973de3a3adb
Arne Mauer <arne@thempolis.nl>
2015-01-06 16:25:11 +0100

    Plugin bestanden aangemaakt.

---
+++ RoottoolsPhonegapPlugin.js
@@ -0,0 +1,85 @@
+cordova.define("cordova/plugin/plot", function(require, exports, module) {
+	plot = {};
+	plot.exampleConfiguration = { "publicKey": "", "cooldownPeriod": -1, "enableOnFirstRun": true };
+    
+  var notificationsToShow = [];
+  var notificationsToFilter = [];
+  
+  var initialized = false;
+    
+	/*
+	Notification: {id: …, message: …, data: …}
+	*/
+    
+	function identityFilterCallback(notifications) {
+		return notifications;
+	}
+  
+  function defaultNotificationHandler(notification, data) {
+  	cordova.exec(undefined, undefined, "PlotCordovaPlugin", "defaultNotificationHandler", [notification, data]);
+  }                            
+   
+ 
+	//only works on IOS
+	plot.filterCallback = identityFilterCallback;
+  
+  plot.notificationHandler = defaultNotificationHandler; 
+  
+  plot._runNotificationHandler = function(notification) {
+  	if (initialized) {
+  		plot.notificationHandler(notification, notification.data);
+  	} else {
+  		notificationsToShow.push(notification);
+  	}
+  };
+  
+  
+	plot._runFilterCallback = function(notifications) {
+		if (initialized) {
+			var result = plot.filterCallback(notifications);
+			cordova.exec(undefined, undefined, "PlotCordovaPlugin", "filterCallbackComplete", [result]);
+		} else {
+			notificationsToFilter.push(notifications);
+		}
+	};  
+  
+    
+	plot.init = function(configuration, successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "initPlot", [configuration]);
+		 initialized = true;
+		 
+		 for (var i = 0; i < notificationsToShow.length; i++) {
+		 		plot._runNotificationHandler(notificationsToShow[i]);  
+		 }
+		 notificationsToShow = [];
+		 
+		 
+		 for (var i = 0; i < notificationsToFilter.length; i++) {
+				plot._runFilterCallback(notificationsToFilter[i]);  
+		 }
+		 notificationsToFilter = [];
+		 
+	};
+	plot.enable = function(successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "enable", []);
+	};
+	plot.disable = function(successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "disable", []);
+	};
+	plot.isEnabled = function(successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "isEnabled", []);
+	};
+	plot.setCooldownPeriod = function(cooldownSeconds, successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "setCooldownPeriod", [cooldownSeconds]);
+	};
+	plot.getVersion = function(successCallback, failureCallback) {
+		 cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "getVersion", []);
+	};
+	
+	//The data for the debug log on iOS is only collected when the DEBUG preprocessor macro is set.
+	plot.mailDebugLog = function(successCallback, failureCallback) {
+		cordova.exec(successCallback, failureCallback, "PlotCordovaPlugin", "mailDebugLog", [])
+	};
+	module.exports = plot;
+});
+
---
+++ src/android/com/rtplugin/RoottoolsPhonegapPlugin.java
@@ -0,0 +1,135 @@
+package com.rtplugin.cordova;
+
+import android.content.Context;
+import android.content.Intent;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.stericson.RootTools.*;
+
+
+public class PlotCordovaPlugin extends CordovaPlugin {
+
+/*
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        handleIntent(cordova.getActivity().getIntent());
+    }
+*/
+
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("rootavailable".equals(action)) {
+            this.isRootAvailable(callbackContext);
+				//} else if ("disable".equals(action)) {
+				//    this.disable(callbackContext);
+				//} else if ("isEnabled".equals(action)) {
+				//    this.isEnabled(callbackContext);
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+/*
+    private void enable(CallbackContext callbackContext) {
+        try {
+            Plot.enable();
+            callbackContext.success();
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+	
+    private void disable(CallbackContext callbackContext) {
+        try {
+            Plot.disable();
+            callbackContext.success();
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+	*/
+
+    private void isRootAvailable(CallbackContext callbackContext) {
+        try {
+            boolean isAvailable = RootTools.isRootAvailable();
+            callbackContext.success(isAvailable ? 1 : 0);
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+	/*
+	
+    private void setCooldownPeriod(JSONArray args, CallbackContext callbackContext) {
+        try {
+            try {
+                int cooldownseconds = args.getInt(0);
+                Plot.setCooldownPeriod(cooldownseconds);
+                callbackContext.success();
+            } catch (JSONException e) {
+                callbackContext.error("Cooldown period not specified or not specified correctly.");
+            }
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void getVersion(CallbackContext callbackContext) {
+        try {
+            String version = Plot.getVersion();
+            callbackContext.success(version);
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void mailDebugLog(CallbackContext callbackContext) {
+        try {
+            Plot.mailDebugLog();
+            callbackContext.success();
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+    private void defaultNotificationHandler(JSONArray args, CallbackContext callbackContext) {
+        try {
+            String data = args.getString(1);
+
+            Uri uri = Uri.parse(data);
+
+            Intent openBrowserIntent = new Intent(Intent.ACTION_VIEW, uri);
+            openBrowserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+
+            Context context = cordova.getActivity();
+            context.startActivity(openBrowserIntent);
+
+            callbackContext.success();
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+
+	
+	*/
+	
+}
+
